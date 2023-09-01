6b55a2fb604b52ba9099284bcdede350375e7b0f
Arie Setiadi <arie@eannovate.com>
2023-09-02 07:35:51 +0800

    adjust production db credentials

--- Menu/Connection.php
+++ Menu/Connection.php
@@ -3,9 +3,9 @@
         private $conn;
 
         protected $host = "localhost";
-        protected $user = "root";
-        protected $pass = "";
-        protected $db = "depot_7";
+        protected $user = "n1566767_root";
+        protected $pass = "@Root1212";
+        protected $db = "n1566767_depot7";
 
         public function __construct(){
             $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->db);
